35430e3d952ce9b19dc10fc0d9b3a5fcc59937b0
AbdelMoad <abdermoad@gmail.com>
2022-10-07 12:39:39 +0200

    java

--- TP2/src/Main.java
+++ TP2/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        parite();
+        egaliteStr();
     }
     public static void discriminant() {
         Scanner scanner = new Scanner(System.in);
@@ -32,9 +32,22 @@ public class Main {
     public static void max() {
         Scanner scanner2 = new Scanner(System.in);
         System.out.println("Selectionner l'entier 1 ?");
-        int entier = scanner2.nextInt();
-        if (entier%2==0) {
-            System.out.println("Cet entier est pair");
-        } else { System.out.println("Cet entier est impair" ); }
+        int entier1 = scanner2.nextInt();
+        System.out.println("Selectionner l'entier 2 ?");
+        int entier2 = scanner2.nextInt();
+        if (entier1 > entier2) {
+            System.out.println("Le maximum est "+ entier1);
+        } else { System.out.println("Le maximum est " + entier2 ); }
     }
+    public static void egaliteStr() {
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Selectionner la premiere chaine de caractère");
+        String chaine1 = scanner2.nextLine();
+        System.out.println("Selectionner la deuxieme chaine de caractère");
+        String chaine2 = scanner2.nextLine();
+        boolean test4 = chaine1.equals(chaine2);
+        System.out.println(test4);
+
+    }
+
 }
