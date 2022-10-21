1959ea2431216b2de45b09cf6b5d948d1fafb726
AbdelMoad <115221817+AbdelMoad@users.noreply.github.com>
2022-10-21 10:04:08 +0200

    Add files via upload

---
+++ TP3-MOAD-G7/src/ASCIIArt.java
@@ -0,0 +1,7 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println(" |\\---/| ");
+        System.out.println(" | 0_0 | ");
+        System.out.println("  \\_^_/  ");
+    }
+}
