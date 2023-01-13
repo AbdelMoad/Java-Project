978f7e8bece9a88ec25d8a4e1c1ec743fe766fba
unicorn03 <valentyna.pronina@nure.ua>
2023-01-13 09:06:23 +0100

    I am working on implementation of classes

--- Game-7Wonders/src/main/java/Classes/ageCard.java
+++ Game-7Wonders/src/main/java/Classes/ageCard.java
@@ -1,2 +1,9 @@
 public abstract class ageCard {
+    private int age;
+    private String name;
+    private String description;
+    private String category;
+    private int minimumPlayers;
+    private boolean direction;
+
 }
