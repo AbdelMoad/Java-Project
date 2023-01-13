f4e9e8db36b3fb3af834a47427eba12d733424a9
unicorn03 <valentyna.pronina@nure.ua>
2023-01-13 10:13:40 +0100

    I am adding my constructor to all classes

--- Game-7Wonders/src/main/java/Classes/yellowCard.java
+++ Game-7Wonders/src/main/java/Classes/yellowCard.java
@@ -3,5 +3,6 @@ import java.util.List;
 public class yellowCard extends ageCard{
     public yellowCard(int a, String n, String d, String c, int mp, boolean dir, List<Resource> r, List<Effect> ef) {
         super(a, n, d, c, mp, dir, r, ef);
+
     }
 }
